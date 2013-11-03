10f97564773f5d944278c3c028c713efdf23e7b9
Essi E Salmenkivi <essalmen@dcb-10.cs.helsinki.fi>
2013-11-03 19:14:05 +0200

    jee

---
+++ src/pale/fire/syntax/Article.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pale.fire.syntax;
+
+/**
+ *
+ * @author essalmen
+ */
+public class Article extends Word {
+    
+}
